fc5c4c4766997d5647340316f6c39453a4d04291
Taylor, Riley Hal <rileytaylor@email.arizona.edu>
2018-10-02 11:12:14 -0700

    fix holderjs

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   title: 'FSO Bootstrap',
   description: 'Bootstrap 4 meets University of Arizona branding',
-  base: '/fso-bootstrap/',
+  base: '',
   dest: 'public',
   head: [
     ['link', { rel: 'icon', href: '/logo.png' }],
@@ -17,11 +17,10 @@ module.exports = {
     ['link', { rel: "stylesheet", href: "https://use.fontawesome.com/releases/v5.3.1/css/solid.css", integrity: "sha384-VGP9aw4WtGH/uPAOseYxZ+Vz/vaTb1ehm1bwx92Fm8dTrE+3boLfF1SpAtB1z7HW", crossorigin: "anonymous" }],
     ['link', { rel: "stylesheet", href: "https://use.fontawesome.com/releases/v5.3.1/css/brands.css", integrity: "sha384-rf1bqOAj3+pw6NqYrtaE1/4Se2NBwkIfeYbsFdtiR6TQz0acWiwJbv1IM/Nt/ite", crossorigin: "anonymous" }],
     ['link', { rel: "stylesheet", href: "https://use.fontawesome.com/releases/v5.3.1/css/fontawesome.css", integrity: "sha384-1rquJLNOM3ijoueaaeS5m+McXPJCGdr5HcA03/VHXxcp2kX2sUrQDmFc3jR5i/C7", crossorigin: "anonymous" }],
-    // Bootstrap
+    // Bootstrap JS
     ['script', { src: 'https://code.jquery.com/jquery-3.3.1.slim.min.js' }],
     ['script', { src: 'https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js' }],
-    ['script', { src: 'https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js' }],
-    // TODO: Need a better way to import these... probably through webpack?
+    ['script', { src: 'https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js' }]
   ],
   serviceWorker: true,
   evergreen: true,
@@ -85,7 +84,8 @@ module.exports = {
       ]
     },
     lastUpdated: 'Last Updated',
-    version: '4.1.3',
+    version: '1.0.0',
+    bootstrapVersion: '4.1.3',
     repo: 'https://gitlab.fso.arizona.edu/FAST/fso-bootstrap',
     repoLabel: 'Source',
     docsDir: 'docs',
--- docs/content/figures.md
+++ docs/content/figures.md
@@ -14,7 +14,7 @@ Use the included `.figure` , `.figure-img` and `.figure-caption` classes to prov
 
 <Example>
   <figure class="figure">
-    <img v-holder="'img=400x300'" class="figure-img img-fluid rounded" alt="A generic square placeholder image with rounded corners in a figure.">
+    <img data-src="holder.js/400x300" class="figure-img img-fluid rounded" alt="A generic square placeholder image with rounded corners in a figure.">
     <figcaption class="figure-caption">A caption for the above image.</figcaption>
   </figure>
 </Example>
@@ -23,7 +23,7 @@ Aligning the figure's caption is easy with our [text utilities]().
 
 <Example>
   <figure class="figure">
-    <img v-holder="'img=400x300'" class="figure-img img-fluid rounded" alt="A generic square placeholder image with rounded corners in a figure.">
+    <img data-src="holder.js/400x300" class="figure-img img-fluid rounded" alt="A generic square placeholder image with rounded corners in a figure.">
     <figcaption class="figure-caption text-right">A caption for the above image.</figcaption>
   </figure>
 </Example>
